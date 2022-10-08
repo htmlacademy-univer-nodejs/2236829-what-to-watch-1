3a00822c4c6f73f214b682dae0bc314673735187
SergeyLiyasov <liseand2002@gmail.com>
2022-10-08 19:54:04 +0500

    mock-data type

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,21 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  publicationDates: string[];
+  genres: string[];
+  releaseYears: number[];
+  ratings: number[];
+  videoPreviewUris: string[];
+  videoUris: string[];
+  casts: string[];
+  producers: string[];
+  durations: number[];
+  commentAmounts: number[];
+  userNames: string[];
+  userAvatarUris: string[];
+  userEmails: string[];
+  userPasswords: string[];
+  posterUris: string[];
+  backgroundImageUris: string[];
+  backgroundColors: string[];
+};

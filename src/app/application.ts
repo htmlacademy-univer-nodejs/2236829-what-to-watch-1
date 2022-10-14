af5721fede97b1c4f464e92f39910f86be933aa5
SergeyLiyasov <liseand2002@gmail.com>
2022-10-15 00:05:52 +0500

    Application.init() логирует $PORT

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,9 +1,14 @@
 import { LoggerInterface } from '../common/logger/logger.interface.js';
+import {ConfigInterface} from '../common/config/config.interface.js';
 
 export default class Application {
-  constructor(private logger: LoggerInterface) {}
+  constructor(
+    private logger: LoggerInterface,
+    private config: ConfigInterface
+  ) {}
 
   public async init() {
     this.logger.info('Инициализация приложения…');
+    this.logger.info(`Значение переменной $PORT: ${this.config.get('PORT')}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Application from './app/application.js';
 import LoggerService from './common/logger/logger.service.js';
+import ConfigService from './common/config/config.service.js';
 
 const logger = new LoggerService();
-const application = new Application(logger);
+const config = new ConfigService(logger);
+const application = new Application(logger, config);
 await application.init();

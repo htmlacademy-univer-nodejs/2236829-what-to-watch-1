641c0ecdb48c5ff0f6a1d70854f4be9eae0ac770
SergeyLiyasov <liseand2002@gmail.com>
2022-10-14 23:52:49 +0500

    ConfigService через dotenv

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,7 +1,20 @@
 import { ConfigInterface } from './config.interface.js';
+import { config, DotenvParseOutput } from 'dotenv';
+import { LoggerInterface } from '../logger/logger.interface.js';
 
 export default class ConfigService implements ConfigInterface {
-  private config: NodeJS.ProcessEnv = process.env;
+  private config: DotenvParseOutput;
+
+  constructor(private logger: LoggerInterface) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Невозможно прочитать файл .env. Возможно, файл не существует.');
+    }
+    
+    this.config = parsedOutput.parsed!;
+    this.logger.info('Файл .env успешно прочитан.');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];

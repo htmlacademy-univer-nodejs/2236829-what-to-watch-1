734aff53c86c57263737c19d4066c14dba3766b8
SergeyLiyasov <liseand2002@gmail.com>
2022-10-23 15:11:07 +0500

    Application.init(): соединение с дб

--- src/app/application.ts
+++ src/app/application.ts
@@ -3,6 +3,8 @@ import { LoggerInterface } from '../common/logger/logger.interface.js';
 import { ConfigInterface } from '../common/config/config.interface.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../types/component.type.js';
+import { getMongoDbURI } from '../utils/db.js';
+import { DatabaseInterface } from '../common/database-client/database.interface.js';
 
 @injectable()
 export default class Application {
@@ -10,11 +12,23 @@ export default class Application {
     @inject(Component.LoggerInterface)
     private logger: LoggerInterface,
     @inject(Component.ConfigInterface)
-    private config: ConfigInterface
+    private config: ConfigInterface,
+    @inject(Component.DatabaseInterface)
+    private databaseClient: DatabaseInterface
   ) {}
 
   public async init() {
     this.logger.info('Инициализация приложения…');
     this.logger.info(`Значение переменной $PORT: ${this.config.get('PORT')}`);
+
+    const uri = getMongoDbURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    await this.databaseClient.connect(uri);
   }
 }

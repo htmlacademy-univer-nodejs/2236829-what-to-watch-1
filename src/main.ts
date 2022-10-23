100f63116cbd2fcead1a1b8dfeda6302afc903e9
SergeyLiyasov <liseand2002@gmail.com>
2022-10-23 23:57:23 +0500

    UserService&Interface

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
+import { types } from '@typegoose/typegoose';
 import Application from './app/application.js';
 import LoggerService from './common/logger/logger.service.js';
 import ConfigService from './common/config/config.service.js';
 import DatabaseService from './common/database-client/database.service.js';
+import UserService from './modules/user/user.service.js';
+import { UserEntity, UserModel } from './modules/user/user.entity.js';
 import { LoggerInterface } from './common/logger/logger.interface.js';
 import { ConfigInterface } from './common/config/config.interface.js';
 import { DatabaseInterface } from './common/database-client/database.interface.js';
+import { UserServiceInterface } from './modules/user/user-service.interface.js';
 import { Component } from './types/component.type.js';
 
 const applicationContainer = new Container();
@@ -14,6 +18,8 @@ applicationContainer.bind<Application>(Component.Application).to(Application).in
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
+applicationContainer.bind<UserServiceInterface>(Component.UserServiceInterface).to(UserService);
+applicationContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();

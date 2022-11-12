6321df4d3fdde5f9cecae34edd31646f04047f9c
SergeyLiyasov <liseand2002@gmail.com>
2022-11-12 15:15:13 +0500

    обновил help-command

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -10,10 +10,10 @@ export default class HelpCommand implements CliCommandInterface {
       Пример:
         ${chalk.yellow('main.js --<command> [--arguments]')}
       Команды:
-        ${chalk.yellow('--version:')}                    ${chalk.blue('# выводит номер версии')}
-        ${chalk.yellow('--help:')}                       ${chalk.blue('# печатает этот текст')}
-        ${chalk.yellow('--import <path>:')}              ${chalk.blue('# импортирует данные из TSV')}
-        ${chalk.yellow('--generate <n> <path> <url>')}   ${chalk.blue('# генерирует произвольное количество тестовых данных')}
+        ${chalk.yellow('--version:')}                                                     ${chalk.blue('# выводит номер версии')}
+        ${chalk.yellow('--help:')}                                                        ${chalk.blue('# печатает этот текст')}
+        ${chalk.yellow('--import <filename> <login> <password> <host> <dbname> <salt>:')} ${chalk.blue('# импортирует данные из TSV и добавляет в базу данных')}
+        ${chalk.yellow('--generate <n> <path> <url>:')}                                   ${chalk.blue('# генерирует произвольное количество тестовых данных')}
       `);
   }
 }

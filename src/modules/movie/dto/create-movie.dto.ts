9ea09c2b2496f43f4e2e0e25699ab5053f88980e
SergeyLiyasov <liseand2002@gmail.com>
2022-11-16 13:01:40 +0500

    fix: CreateMovieDto

--- src/modules/movie/dto/create-movie.dto.ts
+++ src/modules/movie/dto/create-movie.dto.ts
@@ -3,16 +3,14 @@ import { Genre } from '../../../types/genre.type';
 export default class CreateMovieDto {
   public title!: string;
   public description!: string;
-  public publicationDate!: string;
+  public publicationDate!: Date;
   public genre!: Genre;
   public releaseYear!: number;
-  public rating!: number;
   public videoPreviewUri!: string;
   public videoUri!: string;
   public cast!: string[];
   public producer!: string;
   public duration!: number;
-  public commentAmount!: number;
   public userId!: string;
   public posterUri!: string;
   public backgroundImageUri!: string;

f5dc43aefdb4a75a21584986ba631c484c590ae6
SergeyLiyasov <liseand2002@gmail.com>
2022-11-21 03:15:24 +0500

    new ExceptionFilter

--- src/common/errors/exception-filter.ts
+++ src/common/errors/exception-filter.ts
@@ -4,6 +4,8 @@ import { StatusCodes } from 'http-status-codes';
 import { ExceptionFilterInterface } from './exception-filter.interface.js';
 import { Component } from '../../types/component.type.js';
 import { LoggerInterface } from '../logger/logger.interface.js';
+import HttpError from './http-error.js';
+import { createErrorObject } from '../../utils/common.js';
 
 @injectable()
 export default class ExceptionFilter implements ExceptionFilterInterface {
@@ -14,10 +16,25 @@ export default class ExceptionFilter implements ExceptionFilterInterface {
     this.logger.info('Регистрация ExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} — ${error.message}`);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
     this.logger.error(error.message);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({error: error.message});
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    this.handleOtherError(error, req, res, next);
   }
 }

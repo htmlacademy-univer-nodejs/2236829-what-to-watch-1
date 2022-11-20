6370f398b282e015eeeff5239c8e12b5dd0fdf5c
SergeyLiyasov <liseand2002@gmail.com>
2022-11-20 17:46:23 +0500

    Applicatiom.init expressApp

--- src/app/application.ts
+++ src/app/application.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata';
 import { LoggerInterface } from '../common/logger/logger.interface.js';
 import { ConfigInterface } from '../common/config/config.interface.js';
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 import { Component } from '../types/component.type.js';
 import { getMongoDbURI } from '../utils/db.js';
 import { DatabaseInterface } from '../common/database-client/database.interface.js';
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+  
   constructor(
     @inject(Component.LoggerInterface)
     private logger: LoggerInterface,
@@ -15,11 +18,14 @@ export default class Application {
     private config: ConfigInterface,
     @inject(Component.DatabaseInterface)
     private databaseClient: DatabaseInterface
-  ) {}
+  ) {
+    this.expressApp = express();
+  }
 
   public async init() {
     this.logger.info('Инициализация приложения…');
-    this.logger.info(`Значение переменной $PORT: ${this.config.get('PORT')}`);
+    let port = this.config.get('PORT');
+    this.logger.info(`Значение переменной $PORT: ${port}`);
 
     const uri = getMongoDbURI(
       this.config.get('DB_USER'),
@@ -30,5 +36,8 @@ export default class Application {
     );
 
     await this.databaseClient.connect(uri);
+
+    this.expressApp.listen(port);
+    this.logger.info(`Сервер запущен на http://localhost:${port}`);
   }
 }

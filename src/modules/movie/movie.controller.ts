0ba8e35dc23c26280adaf3566d709f9f83309b4e
SergeyLiyasov <liseand2002@gmail.com>
2022-11-21 17:55:02 +0500

    /movies/promo

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -10,6 +10,7 @@ import { fillDto } from '../../utils/common.js';
 import CreateMovieDto from './dto/create-movie.dto.js';
 import { Genre } from '../../types/genre.type.js';
 import MovieDto from './dto/movie.dto.js';
+import { ConfigInterface } from '../../common/config/config.interface.js';
 
 @injectable()
 export default class MovieController extends Controller {
@@ -18,12 +19,15 @@ export default class MovieController extends Controller {
     logger: LoggerInterface,
     @inject(Component.MovieServiceInterface)
     private readonly movieService: MovieServiceInterface,
+    @inject(Component.ConfigInterface)
+    private readonly configService: ConfigInterface,
   ) {
     super(logger);
 
     this.logger.info('Регистрация эндпоинтов для MovieController…');
 
     this.addRoute({path: '/', method: HttpMethod.Get, handler: this.getAll});
+    this.addRoute({path: '/promo', method: HttpMethod.Get, handler: this.getPromo});
     this.addRoute({path: '/:id', method: HttpMethod.Get, handler: this.getById});
     this.addRoute({path: '/', method: HttpMethod.Post, handler: this.create});
     this.addRoute({path: '/:id', method: HttpMethod.Put, handler: this.update});
@@ -45,6 +49,14 @@ export default class MovieController extends Controller {
     res: Response
   ): Promise<void> {
     const movie = await this.movieService.findById(req.params.id);
+    this.ok(res, fillDto(MovieDto, movie));
+  }
+
+  public async getPromo(
+    _req: Request,
+    res: Response
+  ): Promise<void> {
+    const movie = await this.movieService.findById(this.configService.get('PROMO_MOVIE_ID'));
     this.ok(res, fillDto(MovieDto, movie));
   }
 

7e8f39f854478ce940d6631626cfa9d4faccc01b
SergeyLiyasov <liseand2002@gmail.com>
2022-11-21 19:00:16 +0500

    RouteInterface: any→unknown

--- src/types/route.interface.ts
+++ src/types/route.interface.ts
@@ -1,7 +1,7 @@
 import { HttpMethod } from './http-method.enum.js';
 import { NextFunction, Request, Response } from 'express';
 
-export interface RouteInterface<P, ResBody, ReqBody, ReqQuery, Locals extends Record<string, any>> {
+export interface RouteInterface<P, ResBody, ReqBody, ReqQuery, Locals extends Record<string, unknown>> {
   path: string;
   method: HttpMethod;
   handler: (req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>, next: NextFunction) => void;

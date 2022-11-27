50e14e36a0caaddba3c45e43a7f688cc81141632
SergeyLiyasov <liseand2002@gmail.com>
2022-11-27 22:48:08 +0500

    fix: generic параметры в MiddlewareInterface

--- src/types/middleware.interface.ts
+++ src/types/middleware.interface.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
-export interface MiddlewareInterface<P, ResBody, ReqBody, ReqQuery, Locals extends Record<string, unknown>> {
+export interface MiddlewareInterface<
+    P = Record<string, unknown>,
+    ResBody = Record<string, unknown>,
+    ReqBody = Record<string, unknown>,
+    ReqQuery = Record<string, unknown>,
+    Locals extends Record<string, unknown> = Record<string, unknown>
+  > {
   execute(req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>, next: NextFunction): void;
 }

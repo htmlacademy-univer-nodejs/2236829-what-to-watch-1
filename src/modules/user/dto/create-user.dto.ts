b313b764fd8fc295e07b962f8a40d19dc59f05ce
SergeyLiyasov <liseand2002@gmail.com>
2022-11-28 02:12:44 +0500

    валидация CreateUserDto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
+import { MaxLength, MinLength } from "class-validator";
+
 export default class CreateUserDto {
+  @MinLength(1, {message: 'Поле name не может иметь длину меньше 1'})
+  @MaxLength(15, {message: 'Поле name не может иметь длину больше 15'})
   public name!: string;
+
   public avatarUri!: string;
+
   public email!: string;
+
+  @MinLength(6, {message: 'Поле name не может иметь длину меньше 6'})
+  @MaxLength(12, {message: 'Поле name не может иметь длину больше 12'})
   public password!: string;
 }

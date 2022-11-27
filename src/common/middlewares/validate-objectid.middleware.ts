56169c233311775f07d2377c11be75a3734d2bd4
SergeyLiyasov <liseand2002@gmail.com>
2022-11-27 22:53:43 +0500

    fix типы в ValidateObjectIdMiddleware

--- src/common/middlewares/validate-objectid.middleware.ts
+++ src/common/middlewares/validate-objectid.middleware.ts
@@ -2,12 +2,13 @@ import mongoose from 'mongoose';
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { MiddlewareInterface } from '../../types/middleware.interface.js';
-import { ObjectId } from 'bson';
+import { ObjectId, ObjectIdLike } from 'bson';
 import HttpError from '../errors/http-error.js';
 
 const { Types } = mongoose;
 
-export class ValidateObjectIdMiddleware<IdParam extends string> implements MiddlewareInterface<Record<IdParam, ObjectId>> {
+export class ValidateObjectIdMiddleware<IdParam extends string> implements
+    MiddlewareInterface<Record<IdParam, string | number | ObjectId | ObjectIdLike | Buffer | Uint8Array>> {
   constructor(private param: IdParam) {}
 
   public execute(req: Request<Record<IdParam, ObjectId>>, _res: Response, next: NextFunction): void {

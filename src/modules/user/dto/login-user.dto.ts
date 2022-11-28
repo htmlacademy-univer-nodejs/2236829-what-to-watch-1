a788b5b097ee90e7ab1dcef29358a0b625b7d5bc
SergeyLiyasov <liseand2002@gmail.com>
2022-11-28 18:16:55 +0500

    валидация LoginUserDto

--- src/modules/user/dto/login-user.dto.ts
+++ src/modules/user/dto/login-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, MaxLength, MinLength } from "class-validator";
+
 export default class LoginUserDto {
+  @IsEmail({}, {message: 'Значение поля email должно быть валидным адресом электронной почты'})
   public email!: string;
+
+  @MinLength(6, {message: 'Поле password не может иметь длину меньше 6'})
+  @MaxLength(12, {message: 'Поле password не может иметь длину больше 12'})
   public password!: string;
 }

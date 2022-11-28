3c306f7f412c3236d2e27ed903c2fcdf76fd3185
SergeyLiyasov <liseand2002@gmail.com>
2022-11-28 17:57:41 +0500

    fix типы в ValidateDtoMiddleware

--- src/common/middlewares/validate-dto.middleware.ts
+++ src/common/middlewares/validate-dto.middleware.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { ClassConstructor } from 'class-transformer/types/interfaces/class-constructor.type.js';
-import { validate } from 'class-validator';
+import { validate, ValidationError } from 'class-validator';
 import { StatusCodes } from 'http-status-codes';
 import { plainToInstance } from 'class-transformer';
 import { MiddlewareInterface } from '../../types/middleware.interface.js';
 
-export class ValidateDtoMiddleware implements MiddlewareInterface {
-  constructor(private dto: ClassConstructor<object>) {}
+export class ValidateDtoMiddleware<Dto extends object> implements MiddlewareInterface<unknown, ValidationError[], Dto> {
+  constructor(private dto: ClassConstructor<Dto>) {}
 
-  public async execute(req: Request, res: Response, next: NextFunction): Promise<void> {
+  public async execute(req: Request<unknown, ValidationError[], Dto>, res: Response<ValidationError[]>, next: NextFunction): Promise<void> {
     const dtoInstance = plainToInstance(this.dto, req.body);
     const errors = await validate(dtoInstance);
 

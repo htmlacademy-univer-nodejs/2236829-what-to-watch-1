17baa5e91433798376ef577c0b3c21b96e20957b
SergeyLiyasov <liseand2002@gmail.com>
2022-11-28 18:11:59 +0500

    fix again

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -11,7 +11,7 @@ export default class CreateUserDto {
   @IsEmail({}, {message: 'Значение поля email должно быть валидным адресом электронной почты'})
   public email!: string;
 
-  @MinLength(6, {message: 'Поле name не может иметь длину меньше 6'})
-  @MaxLength(12, {message: 'Поле name не может иметь длину больше 12'})
+  @MinLength(6, {message: 'Поле password не может иметь длину меньше 6'})
+  @MaxLength(12, {message: 'Поле password не может иметь длину больше 12'})
   public password!: string;
 }

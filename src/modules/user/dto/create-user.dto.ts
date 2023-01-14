50a1915ec2706535c50da2e71cc4080e488a044e
SergeyLiyasov <liseand2002@gmail.com>
2023-01-14 19:07:19 +0500

    вернул CreateUserDto.avatarUri с дефолтным значением

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,4 +1,5 @@
-import { IsEmail, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import { DEFAULT_AVATAR_FILE_NAME } from '../user.constant';
 
 export default class CreateUserDto {
   @MinLength(1, {message: 'Поле name не может иметь длину меньше 1'})
@@ -7,6 +8,9 @@ export default class CreateUserDto {
 
   @IsEmail({}, {message: 'Значение поля email должно быть валидным адресом электронной почты'})
   public email!: string;
+
+  @IsString({message: 'Поле avatarUri должно быть строкой'})
+  public avatarUri: string = DEFAULT_AVATAR_FILE_NAME;
 
   @MinLength(6, {message: 'Поле password не может иметь длину меньше 6'})
   @MaxLength(12, {message: 'Поле password не может иметь длину больше 12'})

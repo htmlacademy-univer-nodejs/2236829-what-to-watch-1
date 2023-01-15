bf9fff4fa1de9bebb662582ed7aba367e41f269e
SergeyLiyasov <liseand2002@gmail.com>
2023-01-16 00:25:43 +0500

    fix toWatchService.getToWatch

--- src/modules/to-watch/to-watch-service.interface.ts
+++ src/modules/to-watch/to-watch-service.interface.ts
@@ -1,8 +1,9 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { ToWatchEntity } from './to-watch.entity.js';
+import { Populated } from '../../types/populated.type.js';
 
 export interface ToWatchServiceInterface {
-  getToWatch(userId: string): Promise<DocumentType<ToWatchEntity>[]>;
+  getToWatch(userId: string): Promise<Populated<DocumentType<ToWatchEntity>, 'list'> | null>;
   addToToWatch(userId: string, movieId: string): Promise<DocumentType<ToWatchEntity>>;
   deleteFromToWatch(userId: string, movieId: string): Promise<void>;
 }
--- src/modules/to-watch/to-watch.service.ts
+++ src/modules/to-watch/to-watch.service.ts
@@ -3,6 +3,7 @@ import { ToWatchEntity } from './to-watch.entity.js';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { ToWatchServiceInterface } from './to-watch-service.interface.js';
 import { Component } from '../../types/component.type.js';
+import { Populated } from '../../types/populated.type.js';
 
 @injectable()
 export default class ToWatchService implements ToWatchServiceInterface {
@@ -11,9 +12,9 @@ export default class ToWatchService implements ToWatchServiceInterface {
     private readonly toWatchModel: types.ModelType<ToWatchEntity>
   ) {}
 
-  public async getToWatch(userId: string): Promise<DocumentType<ToWatchEntity>[]> {
-    const result = await this.toWatchModel.find({userId}).populate(['list']).exec();
-    return result;
+  public async getToWatch(userId: string): Promise<Populated<DocumentType<ToWatchEntity>, 'list'> | null> {
+    const result = await this.toWatchModel.findOne({userId}).populate('list').exec();
+    return result as Populated<typeof result, 'list'>;
   }
 
   public async addToToWatch(userId: string, movieId: string): Promise<DocumentType<ToWatchEntity>> {

a2204cc95f9ab248b39b3bd16f71f9face253480
SergeyLiyasov <liseand2002@gmail.com>
2023-01-17 15:05:08 +0500

    style fix

--- src/types/middleware.interface.ts
+++ src/types/middleware.interface.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 export interface MiddlewareInterface<
-    P = unknown,
-    ResBody = unknown,
-    ReqBody = unknown,
-    ReqQuery = unknown,
-    Locals extends Record<string, unknown> = Record<string, unknown>
-  > {
+  P = unknown,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery = unknown,
+  Locals extends Record<string, unknown> = Record<string, unknown>
+> {
   execute(req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>, next: NextFunction): void;
 }

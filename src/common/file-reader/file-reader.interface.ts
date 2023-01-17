902f755f30151d0e7c987591afdf4de8d21acc0d
SergeyLiyasov <liseand2002@gmail.com>
2023-01-17 20:33:36 +0500

    fix FileReaderInterface

--- src/common/file-reader/file-reader.interface.ts
+++ src/common/file-reader/file-reader.interface.ts
@@ -1,6 +1,6 @@
 export interface FileReaderInterface {
   readonly filename: string;
   read(): Promise<void>;
-  on(eventName: 'line', listener: (line: string) => void): void;
+  on(eventName: 'line', listener: (line: string, resolve: () => void) => void): void;
   on(eventName: 'end', listener: (count: number) => void): void;
 }

d3c7a25ba984e2dcfc3bcc91af0cd11ddb2aa12c
SergeyLiyasov <liseand2002@gmail.com>
2023-01-17 21:28:31 +0500

    fix noContent

--- src/common/controller/controller.interface.ts
+++ src/common/controller/controller.interface.ts
@@ -9,5 +9,5 @@ export interface ControllerInterface {
   send<T>(res: Response, statusCode: number, data: T): void;
   ok<T>(res: Response, data: T): void;
   created<T>(res: Response, data: T): void;
-  noContent<T>(res: Response, data: T): void;
+  noContent(res: Response): void;
 }

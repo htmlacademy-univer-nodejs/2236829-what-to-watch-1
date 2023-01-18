2db3c96ec515057037583a15c4c64f11285aa4fa
SergeyLiyasov <liseand2002@gmail.com>
2023-01-18 10:49:23 +0500

    fix default curdate

--- src/modules/movie/dto/create-movie.dto.ts
+++ src/modules/movie/dto/create-movie.dto.ts
@@ -11,7 +11,7 @@ export default class CreateMovieDto {
   public description!: string;
 
   @IsDateString({}, {message: 'Поле publicationDate должно быть валидной датой ISO'})
-  public publicationDate: Date = new Date();
+  public publicationDate: string = new Date().toISOString();
 
   @IsIn(GENRES, {message: 'Поле genre должно иметь тип Genre'})
   public genre!: Genre;
